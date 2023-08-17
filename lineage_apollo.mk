84099ed1d776c90a43905a3e458349da2c7d25a9
PiotrZPL <zpl20052020@protonmail.ch>
2023-08-17 20:00:24 +0200

    Added common_full_phone to inherited stuff

--- lineage_apollo.mk
+++ lineage_apollo.mk
@@ -13,7 +13,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/apollo/device.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 TARGET_GAPPS_ARCH := arm64
 
