3747b4d8b48cb2e19093886b63f55f50b84bd155
Piotr Lange <zpl20052020@protonmail.ch>
2026-08-28 00:31:43 +0200

    apollon: Adapt product for ASCP 17

---
+++ apollon.mk
@@ -0,0 +1,30 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit common ASCP configuration.
+$(call inherit-product, vendor/custom/config/common_full_phone.mk)
+
+# Inherit from apollon device.
+$(call inherit-product, device/xiaomi/apollon/device.mk)
+
+ASCP_MAINTAINER := PiotrZPL
+ASCP_MAINTAINER_LINK := https://github.com/PiotrZPL
+
+PRODUCT_NAME := apollon
+PRODUCT_DEVICE := apollon
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Xiaomi Mi 10T
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="apollo_global-user 12 RKQ1.211001.001 V14.0.4.0.SJDMIXM release-keys" \
+    BuildFingerprint=Xiaomi/apollo_global/apollo:12/RKQ1.211001.001/V14.0.4.0.SJDMIXM:user/release-keys
